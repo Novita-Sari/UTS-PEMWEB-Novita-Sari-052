f8231f764b0b7a5f72124b2024818763ca77a9c4
Novita-Sari <82359850+Novita-Sari@users.noreply.github.com>
2022-04-17 22:02:07 +0700

    Add files via upload

---
+++ Soal No 2/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+   <html>
+<?php
+ $prov = array(
+    array(
+      "provinsi" => "Jawa Timur",
+      "kabupaten" => "Surabaya",
+      "kecamatan" => "Jambangan",
+    ),
+    array(
+        "provinsi" => "Jawa Timur",
+        "kabupaten" => "Surabaya",
+        "kecamatan" => "Wonokromo",
+    ),
+    array(
+        "provinsi" => "Jawa Timur",
+        "kabupaten" => "Sidoarjo",
+        "kecamatan" => "Taman",
+    ),
+    array(
+        "provinsi" => "Jawa Timur",
+        "kabupaten" => "Sidoarjo",
+        "kecamatan" => "Waru",
+    ),
+    array(
+        "provinsi" => "Jawa Tengah",
+        "kabupaten" => "Semarang",
+        "kecamatan" => "Kecamatan 1",
+    ),
+    array(
+        "provinsi" => "Jawa Tengah",
+        "kabupaten" => "Semarang",
+        "kecamatan" => "kecamatan 2",
+    ),
+    array(
+        "provinsi" => "Jawa Tengah",
+        "kabupaten" => "Solo",
+        "kecamatan" => "Kecamatan 1",
+    ),
+    array(
+        "provinsi" => "Jawa Tengah",
+        "kabupaten" => "Solo",
+        "kecamatan" => "kecamatan 2",
+    ),
+  );
+
+  
+?>
+<table border ="1" width = "900" align = "center">
+  <thead> 
+  <h2 align="center"> Data Wilayah</h2>
+  <h4 align="center"> by Novita Sari</h4>
+                    
+  <title>No 2_Novita Sari_052</title>
+    <style>
+           thead {
+               background-color: #AAD0FA;
+           }
+       </style>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">Provinsi</th>
+      <th scope="col">Kabupaten</th>
+      <th scope="col">Kecamatan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $no =1;
+    foreach($prov as $b){
+      echo "<tr>";
+      echo "<td>".$no."</td>
+      <td>".$b['provinsi']."</td>
+      <td>".$b['kabupaten']."</td>
+      <td>".$b['kecamatan']."</td>";
+      echo "</tr>";
+      $no++;
+    }
+    ?>
+  </tbody>
+</table>
+</html>
